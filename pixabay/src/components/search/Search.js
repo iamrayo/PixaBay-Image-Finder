acb976a558139c28f36cef9b5470cc7576509ee9
AlphaPaulCode <erioroegbelughepaul@gmail.com>
2024-04-17 11:20:21 +0100

    new

--- pixabay/src/components/search/Search.js
+++ pixabay/src/components/search/Search.js
@@ -14,11 +14,16 @@ import ImageResults from '../image-results/ImageResults'
    }
 
    onTextChange = (e) => {
+    const val = e.target.value;
     this.setState({[e.target.name] : e.target.value}, () => {
+      if(val === ''){
+         this.setState({images: []})
+      }else{
         axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
 
-     .then(res => this.setState({images: res.data.hits}))
-     .catch(err => console.log(err));
+        .then(res => this.setState({images: res.data.hits}))
+        .catch(err => console.log(err));
+      }
     });
    }
 
